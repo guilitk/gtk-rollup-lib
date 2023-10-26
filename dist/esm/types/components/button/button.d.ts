b5fd48a7e6e24a7ddc3f1b00fa7b204b4ea515fb
Guilherme Tomazi Klein <guilhermet.klein@procempa.com.br>
2023-10-26 17:06:24 -0300

    adiciona elimina classnames

--- dist/esm/types/components/button/button.d.ts
+++ dist/esm/types/components/button/button.d.ts
@@ -1,4 +1,4 @@
 import React from "react";
 import { ButtonProps } from "./button.model";
-import "../../procempa-ds.css";
+import "../../procempa-ds.module.css";
 export declare const Button: (props: ButtonProps) => React.JSX.Element;
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import classNames from "classnames";
 import {ButtonProps} from "./button.model";
-import "../../procempa-ds.css"
+import "../../procempa-ds.css";
 
 
 export const Button = (props:ButtonProps) =>{
-    return <button className={classNames("br-button","primary")}><i className={classNames("fa fa-spin fa-spinner")}></i>{props.label}</button>
+    return <button className={"br-button primary"}><i className={"fa fa-spin fa-spinner"}></i>{props.label}</button>
 }
 
