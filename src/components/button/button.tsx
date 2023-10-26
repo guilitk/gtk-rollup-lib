ab88499415522656e2b30a1ea42b1b47ac0194a2
Guilherme Tomazi Klein <guilhermet.klein@procempa.com.br>
2023-10-26 11:57:00 -0300

    added css

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import {ButtonProps} from "./button.model";
+import "./button.css"
 
 export const Button = (props:ButtonProps) =>{
     return <button className={"container"}>{props.label}</button>
