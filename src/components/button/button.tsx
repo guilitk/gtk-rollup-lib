34b28cff2fa35e8ac8a5a43e2365d5fbc915a875
Guilherme Tomazi Klein <guilhermet.klein@procempa.com.br>
2023-10-26 16:07:00 -0300

    adiciona procempa ds

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,8 +1,10 @@
-import React from "react"
+import React from "react";
+import classNames from "classnames";
 import {ButtonProps} from "./button.model";
-import styles from "./button.module.css"
+import "../../procempa-ds.css"
+
 
 export const Button = (props:ButtonProps) =>{
-    return <button className={styles.container}>{props.label}</button>
+    return <button className={classNames("br-button","primary")}><i className={classNames("fa fa-spin fa-spinner")}></i>{props.label}</button>
 }
 
